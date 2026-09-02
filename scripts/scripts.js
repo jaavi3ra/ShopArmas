82a6f1009283d5346ec5747c780d744c8af1a718
jaavi3ra <javi3ra20@gmail.com>
2026-09-02 18:15:10 -0400

    base home

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,36 @@ function inyectarHeader(){
 
 inyectarHeader();
 inyectarFooter();
+
+//
+function cargarContenido(pagina) {
+fetch(pagina)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("content").innerHTML = data;
+})};
+
+cargarContenido("home.html");
+
+//carrito
+   let contador = 0;
+   
+   function agregaritemAlCarrito(boton) {
+        const valor = document.getElementById("cont-carrito");
+        
+        const nombreobjeto = boton.parentElement.querySelector('span').textContent;
+        
+        contador++;
+        
+        valor.innerText = contador; 
+        mensajeSpan(nombreobjeto);   
+}
+
+function mensajeSpan(text) {
+    const mensaje = document.getElementById("mensaje-alerta");
+
+    setTimeout(() => {
+        mensaje.innerText = "Agregaste "+text+" al carrito!.";
+
+    }, 1000);    
+}
