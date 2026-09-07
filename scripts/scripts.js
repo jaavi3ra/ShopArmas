5c338011f5758b9be176ef89c98272d03d338237
asilva-ops <a.silvamuoz@gmail.com>
2026-09-06 23:19:06 -0300

    Ajusta formato del script general

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,32 @@
-function inyectarFooter(){
+function inyectarFooter() {
     document.getElementById("footer").innerHTML = "<p>Tienda de Armas Grand Theft Auto</p>";
 }
 
-function inyectarHeader(){
-    document.getElementById("header").innerHTML = 
-    '<section>'+
-    '<h1 class="prevent-select">GROVE MARKET</h1>'+
-    '</section>'+
-    '<section>'+
-    '<a href="#Catalogo">SHOP</a>'+
-    '<a href="#Categorias">DROPS</a>'+
-    '<a href="#Categorias">ABOUT</a>'+
-    '</section>'+
-    '<section>'+
-    '<p class="prevent-select">YOUR STASH<span class="logo">LOGO</span> <span id="cont-carrito"></span></p>'+
-    '</section>';
+function inyectarHeader() {
+    document.getElementById("header").innerHTML =
+        '<section>' +
+        '<h1 class="prevent-select">GROVE MARKET</h1>' +
+        '</section>' +
+        '<section>' +
+        '<a href="#Catalogo">SHOP</a>' +
+        '<a href="#Categorias">DROPS</a>' +
+        '<a href="#Categorias">ABOUT</a>' +
+        '</section>' +
+        '<section>' +
+        '<p class="prevent-select">YOUR STASH<span class="logo">LOGO</span> <span id="cont-carrito"></span></p>' +
+        '</section>';
 }
 
 inyectarHeader();
 inyectarFooter();
 
-//
+// Carga el contenido del home dentro del contenedor principal.
 function cargarContenido(pagina) {
-fetch(pagina)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("content").innerHTML = data;
-})};
+    fetch(pagina)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("content").innerHTML = data;
+        });
+}
 
 cargarContenido("paginas/home.html");
-
